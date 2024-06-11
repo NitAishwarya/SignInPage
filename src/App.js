00227fcd4184e75124ae50dd1e099a6354d7162a
NitAishwarya <aishwaryachhapparghare2803@gmail.com>
2024-06-11 12:32:21 +0530

    check1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginPage from './Component/LoginPage/LoginPage';
+import horse from './Component/Image/horse-removebg-preview.png';
+
+function App() {
+  return (
+    <div className="App">
+
+  <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
